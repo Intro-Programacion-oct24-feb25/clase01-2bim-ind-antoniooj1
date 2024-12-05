f8cdbcc55ccfb023fdf7eb7d5fe07b6e602c7b55
Antonio Ojeda <ajojeda6@utpl.edu.ec>
2024-12-05 11:05:39 -0500

    cambios

--- ManejoCicloFor/src/paquete01/Ejemplo011.java
+++ ManejoCicloFor/src/paquete01/Ejemplo011.java
@@ -15,11 +15,11 @@ public class Ejemplo011 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        for (int i = 10; i >= 0; i--) {
+        int i;
+        for (i = 10; i >= 0; i--) {
             System.out.printf("%d\n", i);
         }
-        
+        System.out.printf("%d\n", i);
         
     }
     
--- ManejoCicloFor/src/paquete01/Ejemplo07.java
+++ ManejoCicloFor/src/paquete01/Ejemplo07.java
@@ -12,21 +12,31 @@ package paquete01;
 public class Ejemplo07 {
     public static void main(String[] args) {
         int operacion;
+        int operacion2;
+        String mensajesuma = "";
+
         String mensajeFinal = "";
         for (int i = 1; i <= 10; i++) { // for 01
             // System.out.printf("Tabla de multiplicar del número %d\n", i);
             mensajeFinal = String.format("%sTabla de multiplicar del número %d\n", 
                     mensajeFinal, i);
+             mensajesuma = String.format("%sTabla de suma del número %d\n", 
+                    mensajesuma, i);
             for (int contador = 1; contador <= 12; contador++) {
                 operacion = i * contador;
+                operacion2 = i + contador;
+
                 // System.out.printf("%d x %d = %d\n", i, contador, operacion);
                 mensajeFinal = String.format("%s%d x %d = %d\n", 
                         mensajeFinal, i, contador, operacion);
-            }
+                mensajesuma = String.format("%s%d + %d = %d\n", 
+                        mensajesuma, i, contador, operacion2);            }
             // System.out.println("\n");
             mensajeFinal = String.format("%s\n", mensajeFinal);
         } // termina for 01
         
         System.out.printf("%s\n", mensajeFinal);
+        System.out.printf("%s\n", mensajesuma);
+
     }
 }
